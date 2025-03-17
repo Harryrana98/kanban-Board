a090cdd4f25e476cb6db94268d4efb755e3257d0
Harryrana98 <ranaharendra77@gmail.com>
2025-03-17 17:28:20 +0530

    drag and drop updated

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
 
-  const AddButton =document.querySelector(".Add")
-  const inputValue=document.querySelector("#input")
-  const contentDiv=document.querySelector(".content")
+  const AddButtons =document.querySelectorAll(".userInput button")
+  // const inputValue=document.querySelector("#input")
+  const contentDivs =document.querySelectorAll(".content")
 
-
-  AddButton.addEventListener("click", function(){
-    contentDiv.append(inputValue.value)
+  contentDivs.forEach((div)=>{
+    div.addEventListener("drop",dropHandler)
+    div.addEventListener("dragover",dragoverHandler)
   })
+  AddButtons.forEach((btn)=>{
+    btn.addEventListener("click",createTask)
+  })
+  
+  
+  
+  function createTask(e){
+    const btn = e.target;
+    const input = btn.previousElementSibling;
+    console.log(input)
+    const contentDiv = btn.parentElement.previousElementSibling;
+    
+
+    const taskDiv=document.createElement("div")
+    const text=document.createElement("p")
+    const date=document.createElement("span")
+
+    taskDiv.classList.add("task")
+    taskDiv.draggable=true
+    taskDiv.id= Date.now()
+    taskDiv.addEventListener("dragstart",dragstartHandler)
+
+    text.innerHTML=input.value;
+
+    date.classList.add("date")
+    date.innerHTML=new Date().toLocaleString();
+
+
+    taskDiv.append(text,date)
+    contentDiv.append(taskDiv)
+    input.value=""
+
+  }
+
+  function dragstartHandler(e){
+    e.dataTransfer.setData("text/html",e.target.id)
+  }
+
+
+  function dropHandler(e){
+    e.preventDefault();
+    const data=e.dataTransfer.getData("text/html");
+    e.target.appendChild(document.getElementById(data));
+
+  }
+
+  function dragoverHandler(e){
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+  }
